1c1ed46274cd692b13e43aa6e3dddb8853b7ca85
Senthil Kumar Bhaskaran <senthilkumar@Senthils-MacBook-Pro-3.local>
2019-02-08 16:25:22 +0530

    Added the test case for the lib file

--- test/lib/number_to_word_test.rb
+++ test/lib/number_to_word_test.rb
@@ -4,12 +4,35 @@ require "test/unit"
 
 class TestNumberToWord < Test::Unit::TestCase
   def test_letter_combinations
-    final_words = NumberToWord.new()
     combination_words = "8686782825"
+    final_words = NumberToWord.new()
     assert_equal([["unto", "statal"], ""], final_words.letter_combinations(combination_words))
   end
 
   def test_parameter_error
     assert_raise(ArgumentError) { NumberToWord.new("a") }
+  end
+
+  def test_manipulate_results_for_number
+    final_words = NumberToWord.new()
+    keys = [
+      ["t", "u", "v"],
+      ["m", "n", "o"],
+      ["t", "u", "v"],
+      ["m", "n", "o"],
+      ["p", "q", "r", "s"],
+      ["t", "u", "v"],
+      ["a", "b", "c"],
+      ["t", "u", "v"],
+      ["a", "b", "c"],
+      ["j", "k", "l"],
+    ]
+    assert_equal(5, final_words.manipulate_results_for_number(keys).length)
+  end
+
+  def test_arrange_words
+    final_words = NumberToWord.new()
+    results = {2 => [["tot"], []], 3 => [["unto"], ["statal"]], 4 => [[], []], 5 => [[], ["buck"]], 6 => [[], ["taj"]]}
+    assert_equal([["unto", "statal"]], final_words.arrange_words(results))
   end
 end
